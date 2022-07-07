4d2782305326b5c6b4571b657e8ceda052afef9c
astri2704 <astri_012@smkassalaambandung.sch.id>
2022-07-07 16:14:36 +0700

    push projek

--- guru/index.php
+++ guru/index.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
-    <title>Crud</title>
+    <title>Crud Guru</title>
   </head>
   <body>
         <!-- Navbar -->
